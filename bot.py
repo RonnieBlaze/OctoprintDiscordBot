261c7c3aa18090c5c7994b1b5a17c75905a6f493
RonnieBlaze <ron.rolfes@gmail.com>
2018-04-03 09:22:51 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,50 +1,35 @@
 import discord
-import asyncio
-import time
-import urllib.request
-import json
-import platform
 from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
+import time
+
+import urllib.request
 from urllib.request import urlopen
+import json
+import configparser
+import platform
 
-# Variables
-#
+Client = discord.Client()  # Initialise Client
+client = commands.Bot(command_prefix='?')  # Initialise client bot
 
-BotToken = ""    # Discord bot token
-OctoPrintUrl = ""                                      # Octoprints url
-OctoPrintApiKey = ""                        # Octoprints api
-jpgPath = ""                                                        # full path to temporary screenshot i.e /tmp/screen.jpg
-channel = discord.Object(id='')                           # Channel Id you want the bot to reply in
 
-Client = discord.Client()                   #Initialise Client 
-client = commands.Bot(command_prefix = "!") #Initialise client bot
+def load_cfg():
+    global BotToken
+    global OctoPrintUrl
+    global OctoPrintApiKey
+    global jpgPath
 
-#async def my_background_task():
-#    await client.wait_until_ready()
-#    counter = 0
-#    while not client.is_closed:
-#        counter += 1
-#        await client.send_message(channel, counter)
-#        await asyncio.sleep(60) # task runs every 60 seconds
+    cfg = configparser.ConfigParser()
+    cfg.read('config.cfg')
 
-@client.event 
-async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-    print('--------')
-    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-    print('--------')
-    print('Use this link to invite {}:'.format(client.user.name))
-    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-    print('--------')
-    print('Support Discord Server: ')
-    print('Github Link: https://github.com/RonnieBlaze/OctoprintDiscordBot')
-    print('--------')
-    print('You are running ') #Do not change this. This will really help us support you, if you need support.
-    print('Created by ')
-    await client.change_presence(game=discord.Game(name='Started')) #This is buggy, let us know if it doesn't work.
+    OctoPrintUrl = cfg.get('setting', 'url')
+    OctoPrintApiKey = cfg.get('setting', 'apikey')
+    BotToken = cfg.get('setting', 'bot_token')
+    jpgPath = cfg.get('setting', 'jpg_path')
 
-def pbar (precent):
+
+def progress_bar (percent):
     if percent <= 0:
         percent = 0
     elif percent > 100:
@@ -58,49 +43,88 @@ def pbar (precent):
     bar = '[' + bar.ljust(45) + ']  ' + '%.0f%%' % (percent * 100)
     return bar
 
-def OctoRequest(RequestType):
-    if RequestType == "screenshot":
-        uri = "/webcam/?action=snapshot"
-    if RequestType == "jobs":
-        uri = "/api/job?apikey=%s" % (OctoPrintApiKey)
-    if RequestType == "printers":
-        uri = "/api/printer?apikey=%s" % (OctoPrintApiKey)
-    url = OctoPrintUrl + uri
-    return url
 
-def jobDef():
-    u = urlopen(OctoRequest('jobs'))
-    jobapi_dict = json.loads(u.read().decode('utf-8'))
-    state = jobapi_dict['state']
-    if state == "Printing":
-        filename = jobapi_dict['job']['file']['name'][:-6]
-        printTime = jobapi_dict['progress']['printTime']
-        printTimeLeft = jobapi_dict['progress']['printTimeLeft']
-        estimatedPrintTime = jobapi_dict['job']['estimatedPrintTime']
-        completion = jobapi_dict['progress']['completion']
-        convertsec(estimatedPrintTime)
-        mytest = ("```css\nWe are currently printing %s\nElapsed Printing Time: %s\n%s[%.0f%%]\nEstimated Print Time:  %s\nEstimated Time Left:   %s```" % (filename,convertsec(printTime),pbar(completion),completion,convertsec(estimatedPrintTime),convertsec(printTimeLeft)))
-        return (mytest)
-    if state == "Operational":
-        mytest = ("```css\nWe are currently Not Printing```")
-        return (mytest)
+def api_call(request_type):
 
-def printerDef():
-    u = urlopen(OctoRequest('printers'))
-    printer_dict = json.loads(u.read().decode('utf-8'))
-    bedtempactual = printer_dict['temperature']['bed']['actual']
-    bedtemptarget = printer_dict['temperature']['bed']['target']
-    tooltempactual = printer_dict['temperature']['tool0']['actual']
-    tooltemptarget = printer_dict['temperature']['tool0']['target']
-    mytest = ("```css\nBed Temp:  (%sC\%sC)\nNozzel Temp: (%sC\%sC)```" % (bedtempactual,bedtemptarget,tooltempactual,tooltemptarget))
-    return (mytest)
+    if request_type == 'screenshot':
+        uri = '/webcam/?action=snapshot'
+        req = OctoPrintUrl + uri
 
-def convertsec(seconds):
+        try:
+            urllib.request.urlretrieve(req, jpgPath)
+        except urllib.request.HTTPError as e:
+            print('Http Error')
+            print('Error code: ', e.code)
+        except urllib.request.URLError as e:
+            print('URL Error')
+            print('Reason: ', e.reason)
+
+        return
+
+    else:
+        req = OctoPrintUrl + '/api/' + request_type + '?apikey=' + OctoPrintApiKey
+        try:
+            raw = urlopen(req)
+        except urllib.request.HTTPError as e:
+            print('Http Error')
+            print('Error code: ', e.code)
+            return -1
+        except urllib.request.URLError as e:
+            print('URL Error')
+            print('Reason: ', e.reason)
+            return -1
+    return json.loads(raw.read().decode('utf-8'))
+
+
+def get_jobs():
+    response = api_call('jobs')
+
+    if response == -1:
+        status = 'Error retrieving data'
+        return status
+
+    if response['state'] != 'Printing':
+        status = ('```css\n'
+                  'We are currently not printing')
+        return status
+
+    status = ['```css',
+              'We are currently printing ' + response['job']['file']['name'][:-6],
+              'Elapsed Printing Time: ' + convert_sec(response['progress']['printTime']),
+              progress_bar(response['progress']['completion']),
+              'Estimated Time Left: ' + convert_sec(response['job']['estimatedPrintTime']),
+              '```']
+
+    return '\n'.join(status)
+
+
+def get_printers():
+    response = api_call('printers')
+
+    if response == -1:
+        status = 'Error retrieving data'
+        return status
+
+    status = ['```css',
+              '   Bed Temp: ' + str(response['temperature']['bed']['actual']) + '°C',
+              'Target Temp: ' + str(response['temperature']['bed']['target']) + '°C',
+              '¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯',
+              '',
+              'Nozzle Temp: ' + str(response['temperature']['tool0']['actual']) + '°C',
+              'Target Temp: ' + str(response['temperature']['tool0']['target']) + '°C',
+              '¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯',
+              '```']
+
+    return '\n'.join(status)
+
+
+def convert_sec(seconds):
     if seconds >= 86400:
         mins, sec = divmod(seconds, 60)
         hour, mins = divmod(mins, 60)
         day, hour = divmod(hour, 24)
         day, hour = int(day), int(hour)
+
         mins, sec = int(mins), int(sec)
         if day == 1:
             dlabel = ' day '
@@ -124,6 +148,7 @@ def convertsec(seconds):
         mins, sec = divmod(seconds, 60)
         hour, mins = divmod(mins, 60)
         hour, mins = int(hour), int(mins)
+
         sec = int(sec)
         if hour == 1:
             hlabel = ' hour '
@@ -158,20 +183,37 @@ def convertsec(seconds):
             return '%s second' % seconds
         return '%s seconds' % seconds
 
+
+@client.event 
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    print('--------')
+    print('Use this link to invite {}:'.format(client.user.name))
+    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+    print('--------')
+    print('Support Discord Server: ')
+    print('Github Link: https://github.com/RonnieBlaze/OctoprintDiscordBot')
+    print('--------')
+    print('You are running ') #Do not change this. This will really help us support you, if you need support.
+    print('Created by ')
+    await client.change_presence(game=discord.Game(name='Started')) #This is buggy, let us know if it doesn't work.
+
+
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-    if message.content.startswith('!screenshot'):
-        urllib.request.urlretrieve(OctoRequest('screenshot'), jpgPath)
-        await client.send_file(channel, jpgPath)
-    if message.content.startswith('!job'):
-        MyMsg = jobDef()
-        await client.send_message(channel, MyMsg)
-    if message.content.startswith('!temps'):
-        MyMsg = printerDef()
-        await client.send_message(channel, MyMsg)
+    if message.content.upper().startswith('!SCREENSHOT'):
+        api_call('screenshot')
+        await client.send_file(message.channel, jpgPath)
+    if message.content.upper().startswith('!JOB'):
+        MyMsg = get_jobs()
+        await client.send_message(message.channel, MyMsg)
+    if message.content.upper().startswith('!TEMPS'):
+        MyMsg = get_printers()
+        await client.send_message(message.channel, MyMsg)
 
-#client.loop.create_task(my_background_task())
+
+load_cfg()
+
 client.run(BotToken)
